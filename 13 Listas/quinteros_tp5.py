05ab9a7023f744ab5fc12a84b36e07da0956a9a8
Federico Quinteros <fedequinterod30@gmail.com>
2025-05-10 12:50:56 -0300

    Cambio de formato TP 5 Listas

---
+++ 13 Listas/quinteros_tp5.py
@@ -0,0 +1,55 @@
+#Actividad 1
+multiplos4 = list(range(4,101,4))
+print(multiplos4)
+
+#Actividad 2
+miLista = [1,2,3,4,5]
+print(miLista[3])
+
+#Actividad 3
+lista_vacia = []
+lista_vacia.append(1)
+lista_vacia.append(2)
+lista_vacia.append(3)
+print(lista_vacia)
+
+#Actividad 4
+animales = ["perro","gato","conejo","pez"]
+animales[1] = "loro"
+animales[3] = "oso"
+print(animales)
+
+#Actividad 5
+numeros = [8,15,3,22,7]
+numeros.remove(max(numeros))
+print(numeros)
+
+# El programa remueve el número mayor de la lista "numeros"
+
+#Actividad 6
+mi_lista = list(range(10,31,5))
+print(mi_lista[0:2])
+
+#Actividad 7
+autos = ["sedan","polo","suran","gol"]
+autos[1] = "golf"
+autos[2] = "vento"
+print(autos)
+
+#Actividad 8
+dobles = []
+dobles.append(5*2)
+dobles.append(10*2)
+dobles.append(15*2)
+print(dobles)
+
+#Actividad 9
+compras = [["pan", "leche"], ["arroz", "fideos", "salsa"], ["agua"]]
+compras[2].append("jugo")
+compras[1][1] = "tallarines"
+compras[0].remove("pan")
+print(compras)
+
+#Actividad 10
+lista_anidada = [15,True,[25.5,57.9,30.6],False]
+print(lista_anidada)
